a318dd88395e637b9c31a180ac7a41df5be0bcdb
Claire Lawrence <dlawrence154@172-17-46-98.DYNAPOOL.NYU.EDU>
2017-11-21 15:38:27 -0500

    MAYBE everything is working.

--- project.js
+++ project.js
@@ -8,33 +8,30 @@ tileLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/ligh
 map.setView([40.730833, -73.9975], 16);
 let projectFeatures;
 $.getJSON("https://cfl321.github.io/javascripting-english-major-project/geodata.json", function(data){
-  let jsonFile = "https://cfl321.github.io/javascripting-english-major-project/geodata.json";
   let featuresList;
-  featuresList = $.getJSON(jsonFile, function(geoJSON){
-    return geoJSON.features.map(function(feature){
-      return{
-        latitude: feature.geometry.coordinates[1],
-        longitde: feature.geometry.coordinates[0],
-        description: feature.properties.D,
-        fillcolor: feature.properties.E
-      };
-    });
+  featuresList = data.features.map(function(feature){
+    return {
+      latitude: feature.geometry.coordinates[1],
+      longitde: feature.geometry.coordinates[0],
+      description: feature.properties.D,
+      fillColor: feature.properties.E
+    };
   });
-console.log(featuresList);
-featuresList.forEach(function(project){
-  L.circleMarker(
-    [project.latitude, project.longitude],
-    {style: {
-      radius: project.radius,
-      fillColor: project.fillColor
-    }}
-  );
-});
+  console.log(featuresList);
+  featuresList.forEach(function(project){
+    L.circleMarker(
+      [project.latitude, project.longitude],
+      {
+        //radius: project.radius,
+        fillColor: project.fillColor
+      }
+    );
+  });
 });
 let md;
-md = window.markdownit({html: true}).use(window.markdownit);
+md = window.markdownit({html: true}).use(window.markdownitFootnote);
 $.ajax({
-  url: "introduction.md",
+  url: "https://cfl321.github.io/javascripting-english-major-project/introduction.md",
   success: function(markdown){
     let html;
     html = md.render(markdown);
