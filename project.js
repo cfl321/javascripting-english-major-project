bced0b1f6d10ef012f1d53bee9709fc0c330a4c6
Claire Lawrence <dlawrence154@Claires-Air.home>
2017-12-04 22:10:55 -0500

    got the map to appear, but the points still aren't loading

--- project.js
+++ project.js
@@ -32,34 +32,15 @@ $.getJSON("https://cfl321.github.io/javascripting-english-major-project/geodata.
 });
 let md;
 md = window.markdownit({html: true}).use(window.markdownitFootnote);
-$.ajax({
-  url: "https://cfl321.github.io/javascripting-english-major-project/introduction.md",
-  success: function(markdown){
-    let html;
-    html = md.render(markdown);
-    $("#content").html(html);
-<div class="tab-content p-3">
-  <section class="tab-pane active" id="introduction" role="tabpanel"> </section>
-  <section class="tab-pane" id="marina" role="tabpanel"> </section>
-  <section class="tab-pane" id="murry" role="tabpanel"> </section>
-  <section class="tab-pane" id="danielle" role="tabpanel"> </section>
-  <section class="tab-pane" id="julius" role="tabpanel"> </section>
-  <section class="tab-pane" id="bootie" role="tabpanel"> </section>
-  <section class="tab-pane" id="david" role="tabpanel"> </section>
-  <section class="tab-pane" id="ludo" role="tabpanel"> </section>
-  <section class="tab-pane" id="conclusion" role="tabpanel"> </section>
-</div>
-  }
-  md = window.markdownit({html: true}).use(window.markdownitFootnote);
 ["introduction", "marina",
   "murry", "danielle",
   "julius", "bootie", "david", "ludo", "conclusion"].forEach(function(tab){
-  $.ajax({
-    url: tab + ".md",
-    success: function(markdown){
-      let html;
-      html = md.render(markdown);
-      $("#" + tab).html(html);
-    }
+$.ajax({
+  url: tab + ".md",
+  success:function(markdown){
+    let html;
+    html = md.render(markdown);
+    $("#content").html(html);
+  }
   });
 });
