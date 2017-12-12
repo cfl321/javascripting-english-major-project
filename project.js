64aa12deb65842b8abab78e0008f559b0e9a8f6a
Claire Lawrence <dlawrence154@Claires-Air.home>
2017-12-12 17:01:31 -0500

    edit pop up

--- project.js
+++ project.js
@@ -15,59 +15,27 @@ $.getJSON("https://cfl321.github.io/javascripting-english-major-project/geodata.
       longitude: feature.geometry.coordinates[0],
       picture: feature.properties.E,
       fillColor: feature.properties.D,
-      title: feature.properties.A
+      name: feature.properties.A
     };
   });
   console.log(featuresList);
   let featuresLayer;
   featuresLayer = L.featureGroup(featuresList.map(function(project){
-    return L.circleMarker(
+    let popupContent;
+      popupContent = "<h4>" + feature.name + "</h4>";
+      popupContent = popupContent + "This is a picture of" + feature.name + " on <a href='"+ feature.picture + "'>Wikipedia</a>.";
+      return L.circleMarker(
       [project.latitude, project.longitude] ,
       {
         fillColor: project.fillColor
       }
-    );
+    ).bindPopup(popupContent);
   }));
   featuresLayer.addTo(map);
   //console.log(featuresLayer.getBounds());
   map.fitBounds(featuresLayer.getBounds());
   //console.log(featuresLayer);
-  projectLayer = L.featureGroup(projectFeatures.map(function(feature){
-let juliusPoint, thwaitePoint, daniellePoint, officePoint, davidPoint, ludowPoint, stocktonPoint, worldtradePoint, bootiePoint, miamiPoint;
-    juliusPoint = L.marker([40.718285, -73.982449]);
-    juliusPoint.addTo(map);
-    juliusPoint.bindPopup("This is Julius's Apartment.");
-    thwaitePoint = L.marker([40.784418, -73.970089]);
-    thwaitePoint.addTo(map);
-    thwaitePoint.bindPopup("This is the Thwaite Family Apartment.");
-    daniellePoint = L.marker([40.735928, -73.997598]);
-    daniellePoint.addTo(map);
-    daniellePoint.bindPopup("This is Danielle's Apartment.");
-    officePoint = L.marker([40.725944, -73.994615]);
-    officePoint.addTo(map);
-    officePoint.bindPopup("This is Danielle's Office.");
-    davidPoint = L.marker([40.746384, -74.004335]);
-    davidPoint.addTo(map);
-    davidPoint.bindPopup("This is David's Apartment.");
-    ludowPoint = L.marker([40.737569, -73.985989]);
-    ludowPoint.addTo(map);
-    ludowPoint.bindPopup("This is Ludow's Apartment.");
-    stocktonPoint = L.marker([42.287587, -73.320386]);
-    stocktonPoint.addTo(map);
-    stocktonPoint.bindPopup("This is the Thwaite family's summer home in Stockton, MA..");
-    worldtradePoint = L.marker([40.712625, -74.012886]);
-    worldtradePoint.addTo(map);
-    worldtradePoint.bindPopup("This is the World Trade Center.");
-    bootiePoint = L.marker([40.695231, -73.974274]);
-    bootiePoint.addTo(map);
-    bootiePoint.bindPopup("This is Bootie's Apartment.");
-    miamiPoint = L.marker([25.770024, -80.202596]);
-    miamiPoint.addTo(map);
-    miamiPoint.bindPopup("This is where Bootie relocates to after the events of September 11, 2001.");
-let popupContent;
-  popupContent = "<h4>" + feature.name + "</h4>";
-  popupContent = popupContent + "Read about " + feature.name + " on <a href='"+ feature.picture + "'>Wikipedia</a>.";
-  return L.marker(feature.latLng).bindPopup(popupContent);
+  projectLayer = L.featureGroup(featuresList.map(function(feature){
   })
 );
 });
